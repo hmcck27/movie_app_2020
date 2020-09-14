27e74c2a591522ef33586fdb9d27e47bdb6ccd00
jinkyu <you@example.com>
2020-09-14 12:30:48 +0900

    Updates

---
+++ precache-manifest.c074b02a3139fb3ab0f79b0831842093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436f2efdd4b0d6a5be669199cf704548",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "8687f7b4582d4634fed7",
+    "url": "/movie_app_2020/static/css/main.8faed522.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8687f7b4582d4634fed7",
+    "url": "/movie_app_2020/static/js/main.0447344a.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
